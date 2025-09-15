c7e338e4cdaeca77f180b8eabf4fe4bbe608782c
SamoLibre <samilberslan@gmail.com>
2025-09-15 11:39:53 +0300

    Fix Tpsaa error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from 'react-router-dom';
 import { SelectedDateProvider } from './context/SelectedDateContext';
 
